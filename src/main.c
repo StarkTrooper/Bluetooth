8468d401a0994d890efe215c102ff2b0a7f32b85
Gaddiel Ramos <97007510+StarkTrooper@users.noreply.github.com>
2023-08-09 13:42:44 -0600

    inital commit

--- src/main.c
+++ src/main.c
@@ -4,10 +4,158 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf.h"
+#include "nordic_common.h"
+#include "boards.h"
+#include "nrf_delay.h"
+#include "nrf_log.h"
+#include "app_uart.h"
+#include "app_error.h"
+#include "app_util_platform.h"
+#include "bsp_btn_ble.h"
+#include "ble.h"
+#include "ble_hci.h"
+#include "ble_advdata.h"
+#include "ble_advertising.h"
+#include "ble_conn_params.h"
+#include "nrf_ble_gatt.h"
+
+#define APP_BLE_CONN_CFG_TAG        1 /**< A tag identifying the SoftDevice BLE configuration. */
+
+static ble_gap_adv_params_t m_adv_params; /**< Parameters to be passed to the stack when starting advertising. */
+static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID; /**< Handle of the current connection. */
+
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_sdh_enable_request();
+    APP_ERROR_CHECK(err_code);
+
+    // Fetch the start address of the application RAM.
+    uint32_t ram_start = 0;
+    err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_sdh_ble_enable(&ram_start);
+    APP_ERROR_CHECK(err_code);
+}
+
+/**@brief Function for initializing the Advertising functionality.
+ */
+static void advertising_init(void)
+{
+    ret_code_t             err_code;
+    ble_advertising_init_t init;
+
+    memset(&init, 0, sizeof(init));
+
+    init.advdata.name_type             = BLE_ADVDATA_FULL_NAME;
+    init.advdata.include_appearance    = true;
+    init.advdata.flags                 = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
+
+    init.config.ble_adv_fast_enabled  = true;
+    init.config.ble_adv_fast_interval = MSEC_TO_UNITS(100, UNIT_0_625_MS);
+    init.config.ble_adv_fast_timeout  = APP_ADV_TIMEOUT_IN_SECONDS;
+
+    err_code = ble_advertising_init(&m_advertising, &init);
+    APP_ERROR_CHECK(err_code);
+
+    ble_advertising_conn_cfg_tag_set(&m_advertising, APP_BLE_CONN_CFG_TAG);
+}
+
+/**@brief Function for handling events from the BLE stack.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ */
+void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
+{
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_CONNECTED:
+            m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+            break;
+
+        case BLE_GAP_EVT_DISCONNECTED:
+            m_conn_handle = BLE_CONN_HANDLE_INVALID;
+            break;
+
+        default:
+            // No implementation needed.
+            break;
+    }
+}
+
+/**@brief Function for handling the Application's BLE Stack events.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ * @param[in]   p_context   Unused.
+ */
+static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
+{
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_CONNECTED:
+            m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+            break;
+
+        case BLE_GAP_EVT_DISCONNECTED:
+            m_conn_handle = BLE_CONN_HANDLE_INVALID;
+            break;
+
+        default:
+            // No implementation needed.
+            break;
+    }
+}
+
+/**@brief Function for handling events from the GATT library.
+ *
+ * @param[in] p_gatt   GATT library event.
+ */
+static void gatt_evt_handler(nrf_ble_gatt_t * p_gatt, nrf_ble_gatt_evt_t const * p_evt)
+{
+    if ((m_conn_handle == p_evt->conn_handle) && (p_evt->evt_id == NRF_BLE_GATT_EVT_ATT_MTU_UPDATED))
+    {
+        // MTU size changed
+    }
+}
+
+/**@brief Function for initializing the GATT library.
+ */
+static void gatt_init(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_ble_gatt_init(&m_gatt, gatt_evt_handler);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_ble_gatt_att_mtu_periph_set(&m_gatt, NRF_SDH_BLE_GATT_MAX_MTU_SIZE);
+    APP_ERROR_CHECK(err_code);
+}
 
 int main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
-	return 0;
+    // Initialize
+    log_init();
+    ble_stack_init();
+    gap_params_init();
+    gatt_init();
+    advertising_init();
+    conn_params_init();
+
+    // Start advertising
+    ble_advertising_start(&m_advertising, BLE_ADV_MODE_FAST);
+
+    while (true)
+    {
+        // Your application logic here
+        nrf_pwr_mgmt_run();
+    }
 }
